e3d76e22f451b6cf1bde04e35e0b1f96aab3f744
任婉君 <renwanjun@iMac-2.local>
2018-01-31 19:19:45 +0800

    1.添加浏览器类别判断2.调整heigh

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,49 @@
 $(document).ready(function () {
+    // var u = navigator.userAgent
+    // // if (version && version < 4.3) {
+    // var obj = {
+    //     title: '我的美篇成长之旅',
+    //     desc: desc,
+    //     image: 'https://ss2.meipian.me/theme/v2/activity/my2017logo.png',
+    //     url: window.location.href
+    // }
+    // if (!!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)) {
+    //     // ios
+    //     // window.android && window.android.sharePopup(JSON.stringify(obj))
+    //     window.webkit.messageHandlers.sharePopup.postMessage(JSON.stringify(obj))
+    // } else if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {
+    //     //安卓
+    //     window.android && window.android.sharePopup(JSON.stringify(obj))
+    // }
+    $("img").click(function (evt) {
+        evt.preventDefault();
+    });
     $("img.lazy-load").lazyload({
         effect: "fadeIn", //渐现，show(直接显示),fadeIn(淡入),slideDown(下拉)
         threshold: 180, //预加载，在图片距离屏幕180px时提前载入
         // event: 'scroll',  // 事件触发时才加载，click(点击),mouseover(鼠标划过),sporty(运动的),默认为scroll（滑动）
         // container: $("#container"), // 指定对某容器中的图片实现效果
-        'failure_limit':2 //加载2张可见区域外的图片,lazyload默认在找到第一张不在可见区域里的图片时则不再继续加载,但当HTML容器混乱的时候可能出现可见区域内图片并没加载出来的情况
+        'failure_limit': 2 //加载2张可见区域外的图片,lazyload默认在找到第一张不在可见区域里的图片时则不再继续加载,但当HTML容器混乱的时候可能出现可见区域内图片并没加载出来的情况
     });
-
     $("img.lazy-load").lazyload({
         effect: "fadeIn", //渐现，show(直接显示),fadeIn(淡入),slideDown(下拉)
         // threshold: 180, //预加载，在图片距离屏幕180px时提前载入
-        event: 'clisk',  // 事件触发时才加载，click(点击),mouseover(鼠标划过),sporty(运动的),默认为scroll（滑动）
-        container: $(".list-influentialauthor"), // 指定对某容器中的图片实现效果
+        event: 'click',  // 事件触发时才加载，click(点击),mouseover(鼠标划过),sporty(运动的),默认为scroll（滑动）
+        container: $(".list"), // 指定对某容器中的图片实现效果
         // 'failure_limit':2 //加载2张可见区域外的图片,lazyload默认在找到第一张不在可见区域里的图片时则不再继续加载,但当HTML容器混乱的时候可能出现可见区域内图片并没加载出来的情况
     });
 
-    $('a.goto-article,a.goto-column').click(function(evt){
-        evt.preventDefault();
-        var href=$(this).attr('href').trim();
-        var className=$(this).attr('class');
 
-        if(!call_app_pure.isAPP){
-            window.location.href=href;
+    $('a.goto-article,a.goto-column').click(function (evt) {
+        evt.preventDefault();
+        var href = $(this).attr('href').trim();
+        var className = $(this).attr('class');
+
+        if (!call_app_pure.isAPP) {
+            window.location.href = href;
             return;
         }
-        
+
         // 上线环境
         // var start=href.lastIndexOf('\/')+1,
         //     end=href.length-1;
@@ -40,20 +59,20 @@ $(document).ready(function () {
         // }
 
         // 测试环境
-        var start=href.lastIndexOf('\/')+1,
-            end=href.length-1;
+        var start = href.lastIndexOf('\/') + 1,
+            end = href.length - 1;
 
-        if(className.indexOf('article')>=0){
-            href='https://t-www.meipian.cn/v8k1irx';
-            var start=href.lastIndexOf('\/')+1,
-            end=href.length-1;
-            var mask_id=href.substr(start,end);
+        if (className.indexOf('article') >= 0) {
+            href = 'https://t-www.meipian.cn/v8k1irx';
+            var start = href.lastIndexOf('\/') + 1,
+                end = href.length - 1;
+            var mask_id = href.substr(start, end);
             call_app_pure.goArticle(mask_id);
-        }else if(className.indexOf('column')>=0){
-            href='https://t-www.meipian.cn/c/15895943';
-            var start=href.lastIndexOf('\/')+1,
-            end=href.length-1;
-            var user_id=href.substr(start,end);
+        } else if (className.indexOf('column') >= 0) {
+            href = 'https://t-www.meipian.cn/c/15895943';
+            var start = href.lastIndexOf('\/') + 1,
+                end = href.length - 1;
+            var user_id = href.substr(start, end);
             call_app_pure.goColumn(user_id);
         }
 
@@ -83,15 +102,14 @@ $(document).ready(function () {
     if (call_app_pure.isAPP) {
         $('.footer').hide();
     }
-
     // 查看更多事件
     $('.readmore a').click(function () {
         $(this).parent().hide();
         $(this).parent().siblings().show();
     })
 
-    // 导航切换
-    $('.nav').on('click', 'a', function () {
+    // 导航切换点击事件
+    $('.nav').on('click', 'span', function () {
         var href = $(this).attr('href');
         if (!/^\#/.test(href)) return;
         href = href.replace(/\#/, '').trim();
@@ -99,21 +117,22 @@ $(document).ready(function () {
         if (present.indexOf(href) >= 0) return;
         $(this).parent().attr('class', 'nav ' + href);
 
-        // 标题切换
+        // 标题切换 
         $(this).parent().next().children().hide();
         $(this).parent().next().children('.' + href).show();
 
         // 内容切换
         $(this).parent().siblings('.list').hide();
-        
-        var $next=$(this).parent().siblings('.list .list-' + href);
-        $next.show();
-        window.scrollTo(window.scrollX,window.scrollY+2);
-        // $next.children('img.lazyload').each(function(){
-        //     var src=$(this).
-        // })
 
-        
+        var $next = $(this).parent().siblings('.list .list-' + href);
+        $next.show();
+        window.scrollTo(window.scrollX, window.scrollY + 2);
+
+        // $("img.lazy-load").lazyload({
+        //     effect: "fadeIn", //渐现，show(直接显示),fadeIn(淡入),slideDown(下拉)
+        //     container: $next, // 指定对某容器中的图片实现效果
+        // });
+    
     });
 
     $('.footer').click(function () {
